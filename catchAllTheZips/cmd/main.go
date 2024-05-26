466ff0de11b62a4261eac636bbb6818a0e567017
Luciano Santos <luciano_z7@hotmail.com>
2024-05-26 12:20:33 -0300

    Initializing catch All the zip codes

---
+++ catchAllTheZips/cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	http.ListenAndServe(":8080", mux)
+	log.Println("Server started on :8080")
+
+}
---
+++ catchAllTheZips/go.mod
@@ -0,0 +1,3 @@
+module github.com/GoExpertCurso/catchAllTheZips
+
+go 1.22.2
